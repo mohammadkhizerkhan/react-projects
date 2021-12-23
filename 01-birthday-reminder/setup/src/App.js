6573035dc03994b3338bc3ec6f85355739376c40
mohammadkhizerkhan <64767673+mohammadkhizerkhan@users.noreply.github.com>
2021-12-23 15:53:50 +0530

    completed birthday project

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -2,7 +2,21 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [people, setpeople] = useState(data)
+  console.log(people)
+  return (
+    <main>
+      <div className="container">
+        <h3>{people.length} birthdays today</h3>
+        {
+          people.map((person)=>{
+            return <List key={person.id} {...person}/>
+          })
+        }
+        <button className="btn" onClick={()=>setpeople([])}>Clear All</button>
+      </div>
+    </main>
+  )
 }
 
 export default App;
--- 01-birthday-reminder/setup/src/List.js
+++ 01-birthday-reminder/setup/src/List.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
-const List = () => {
+const List = ({name,age,image}) => {
   return (
     <>
-      <h2>list component</h2>
+    <article className="person">
+      <img src={image} alt="name" />
+      <div>
+      <h4>{name}</h4>
+      <p>{age} years</p>
+      </div>
+    </article>
     </>
   );
 };
